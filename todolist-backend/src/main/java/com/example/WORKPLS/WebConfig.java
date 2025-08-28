bc3e88f2e98da2d3b2455e0b8ab2d3b609180d06
setthawutK <setthawut.bao.work@gmail.com>
2025-08-28 23:38:21 +0700

    fix : cors

--- todolist-backend/src/main/java/com/example/WORKPLS/WebConfig.java
+++ todolist-backend/src/main/java/com/example/WORKPLS/WebConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // ครอบคลุมทุก path
-                .allowedOrigins("http://localhost:3000") 
-                .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
+        registry.addMapping("/**") // ครอบทุก endpoint
+                .allowedOrigins("http://localhost:4400", "http://54.169.241.164:3000") // FE ที่อนุญาต
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
