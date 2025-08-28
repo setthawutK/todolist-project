7d9cbcaf968a79cd7ed5edf854c6632c11d08bf1
setthawutK <setthawut.bao.work@gmail.com>
2025-08-29 01:49:42 +0700

    fix : core patch

--- todolist-backend/src/main/java/com/example/WORKPLS/WebConfig.java
+++ todolist-backend/src/main/java/com/example/WORKPLS/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // ครอบทุก endpoint
                 .allowedOrigins("http://localhost:4400", "http://54.169.241.164:3000") // FE ที่อนุญาต
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
