d4273364f07399278f6cd604ed733e05309a5adb
setthawutK <setthawut.bao.work@gmail.com>
2025-08-29 00:26:09 +0700

    fix : reg

--- todolist-backend/src/main/java/com/example/WORKPLS/dto/ApiResponse.java
+++ todolist-backend/src/main/java/com/example/WORKPLS/dto/ApiResponse.java
@@ -2,11 +2,13 @@ package com.example.WORKPLS.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@AllArgsConstructor   // ✅ generate constructor(success, message, username)
+@NoArgsConstructor    // ✅ generate default constructor (ไม่มี args)
 public class ApiResponse {
     private boolean success;
     private String message;
-    private String username; 
+    private String username;
 }
